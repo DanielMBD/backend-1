d53a349fbbca9bc7d5caea6e66dc52cd5f4a1bcf
DanielMb <Dapierre25@gmail.com>
2025-09-26 01:52:58 +0100

    c

--- middleware/cors.js
+++ middleware/cors.js
@@ -22,7 +22,7 @@ const corsOptions = {
     'http://127.0.0.1:8086',  'http://localhost:8089',
     'http://127.0.0.1:8086',  'http://localhost:8089',
     'http://127.0.0.1:8087',  'http://localhost:8090',
-    'http://127.0.0.1:8087',  'http://localhost:8090',
+    'http://127.0.0.1:8087',  'https://gabonconcours.netlify.app',
     'http://127.0.0.1:8088',
   ],
   credentials: true,
